839226e1ccce5f398e3ca3fcc1cbbaa3f03a4b4f
Thiago Garcia <thiagosonnesso@hotmail.com>
2018-08-23 11:17:46 -0300

    Estudo node

--- curso-node/app.js
+++ curso-node/app.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res) {
-    res.send("<html><body>Portal de Noticias</body></html>")
+    res.render("home/index")
 });
 
-app.get('/tecnologia', function(req, res) {
-    res.send("<html><body>Portal de Tecnologia</body></html>")
+app.get('/formulario_inclusao_noticia', function(req, res) {
+    res.send("admin/form_add_noticia")
+});
+
+app.get('/noticias', function(req, res) {
+    res.send("noticias/noticias")
 });
 
 app.listen(3000, function() {
