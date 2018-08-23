d308d5779c308444d87a197daa543298344d8a46
Thiago Garcia <thiagosonnesso@hotmail.com>
2018-08-23 11:04:40 -0300

    aprendendo organização express

--- curso-node/app.js
+++ curso-node/app.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.get('/', function(req, res) {
+    res.send("<html><body>Portal de Noticias</body></html>")
+});
+
+app.get('/tecnologia', function(req, res) {
+    res.send("<html><body>Portal de Tecnologia</body></html>")
+});
+
 app.listen(3000, function() {
     console.log('Server On');
 });
