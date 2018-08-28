779fd0fc32f349891e1fa81b779d10adb9284098
Thiago Garcia <thiagosonnesso@hotmail.com>
2018-08-28 10:12:26 -0300

    Estudo Node / reorganização de module.exports

--- curso-node/app.js
+++ curso-node/app.js
@@ -1,20 +1,14 @@
-var express = require('express');
-var app = express();
+var app = require('./config/server');
 
-app.set('view engine', 'ejs');
+var rotaNoticias = require('./app/routes/noticias');
+rotaNoticias(app);
 
-app.get('/', function(req, res) {
-    res.render("home/index")
-});
+var rotaHome = require('./app/routes/home');
+rotaHome(app);
 
-app.get('/formulario_inclusao_noticia', function(req, res) {
-    res.render("admin/form_add_noticia")
-});
-
-app.get('/noticias', function(req, res) {
-    res.render("noticias/noticias")
-});
+var rotaFormulario = require('./app/routes/formulario_inclusao_noticia');
+rotaFormulario(app);
 
 app.listen(3000, function() {
-    console.log('Server On');
+    console.log('Server ON');
 });
--- curso-node/requisicoeshttp.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-
-http.createServer(function (req, res) {
-    res.end("<html><body>Portal de Noticias</body></html>");
-
-}).listen(3000);
