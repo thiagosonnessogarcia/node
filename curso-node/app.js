714ea69bc74abb5a4393dff70be408e1fa22e754
Thiago Garcia <thiagosonnesso@hotmail.com>
2018-08-28 17:02:27 -0300

    Refactoring

--- curso-node/app.js
+++ curso-node/app.js
@@ -1,13 +1,13 @@
 var app = require('./config/server');
 
-var rotaNoticias = require('./app/routes/noticias');
-rotaNoticias(app);
+//var rotaNoticias = require('./app/routes/noticias');
+//rotaNoticias(app);
 
-var rotaHome = require('./app/routes/home');
-rotaHome(app);
+//var rotaHome = require('./app/routes/home');
+//rotaHome(app);
 
-var rotaFormulario = require('./app/routes/formulario_inclusao_noticia');
-rotaFormulario(app);
+//var rotaFormulario = require('./app/routes/formulario_inclusao_noticia');
+//rotaFormulario(app);
 
 app.listen(3000, function() {
     console.log('Server ON');
