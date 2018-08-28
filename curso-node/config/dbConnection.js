a029e4a97b0c0aa842cc0f245b6e4a375e67f04f
Thiago Garcia <thiagosonnesso@hotmail.com>
2018-08-28 17:10:12 -0300

    Refactoring

--- curso-node/config/dbConnection.js
+++ curso-node/config/dbConnection.js
@@ -1,10 +1,16 @@
 var mysql = require('mysql');
 
-module.exports = function () {
+var connMySQL = function(){
+    console.log('Conexão com BD foi Estabelecida')
     return mysql.createConnection({
         host: 'localhost',
         user: 'root',
         password: '1234',
         database: 'portal_noticias'
     });
+}
+
+module.exports = function () {
+    console.log('O Autoload carregou com sucesso!!! BD')
+    return connMySQL;
 }
