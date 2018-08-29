7f4a9427b4e4e2f6b57b1054526370a5591047bd
Thiago Garcia <thiagosonnesso@hotmail.com>
2018-08-29 15:22:48 -0300

    Organizando o código

--- curso-node/app/models/noticiasModel.js
+++ curso-node/app/models/noticiasModel.js
@@ -1,11 +1,15 @@
 module.exports = function() {
 
     this.getNoticias = function(connection, callback) {
-        connection.query('select * from noticias', callback);
+        connection.query('SELECT * FROM noticias', callback);
     }
 
     this.getNoticia = function(connection, callback){
-        connection.query('select * from noticias where id_noticia = 2', callback);
+        connection.query('SELECT * FROM noticias where id_noticia = 2', callback);
+    }
+
+    this.salvarNoticia = function(noticia, connection, callback){
+        connection.query('INSERT INTO noticias SET ? ', noticia, callback)
     }
     return this;
 }
